86fa2981fef1a7f2d6b92e145db9f60b7e32c1de
Karim Rashad <karimrashad.official@yahoo.com>
2025-05-21 15:34:25 +0300

    `Added flutter_gen_runner and flutter_svg dependencies, updated pubspec.yaml and pubspec.lock files, and modified main.dart to use AssetsApp.icons.logo.svg() in MaterialApp home widget.`

--- stylish_app/lib/main.dart
+++ stylish_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stylish_app/core/constants/assets.gen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(home: Scaffold(body: AssetsApp.icons.logo.svg()));
   }
 }
